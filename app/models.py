f47c074321201810536334ee96a583214605e5dc
alexey <user@mail.ru>
2026-08-12 11:43:06 +0300

    start creating model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Ware(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    inventory = models.CharField(max_length=50, unique=True, null=True)
+    serial = models.CharField(max_length=50, null=True)
+    quantity = models.FloatField(default=1.)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
